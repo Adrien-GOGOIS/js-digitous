6d086957c53dc3f60122affd9048ec87f692d948
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-13 13:53:12 +0100

    first commit

---
+++ jour01.js
@@ -0,0 +1,18 @@
+// 01 - Hello World
+
+console.log('Hello World!');
+
+// 02 - String
+
+var test = 'My name is Adrien';
+console.log(test);
+
+// 03 - Concatenation
+
+var name = 'Adrien';
+console.log(`Nice to meet you ${name}`);
+
+// 04 - String lenght
+
+var testLenght = "I'm very long !";
+console.log(testLenght.length);
