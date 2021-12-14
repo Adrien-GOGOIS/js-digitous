a0cbfcc4db0ec6bccf1510c19f9818c0ac1b8243
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-14 13:46:54 +0100

    add exercices jour02

---
+++ jour02.js
@@ -0,0 +1,28 @@
+// 01 - Number
+
+let integer = 102;
+let float = 13.9;
+console.log(integer, float);
+
+// 02 - Convert
+
+let basic = 34;
+let stringified = basic.toString();
+console.log(stringified);
+
+// 03 - Round
+
+let num = 1.5;
+let rounded = Math.round(num);
+console.log(rounded);
+
+// 04 - Arithmétique
+
+let test = 12;
+let bis = 5;
+console.log(test + bis);
+console.log(test - bis);
+console.log(test * bis);
+console.log(test / bis);
+console.log(test ** bis);
+console.log(test % bis);
