e6eea3f4b8f464a9498d598d5de6be8014daffe4
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-15 14:05:01 +0100

    add jour03 exo2-3-4

---
+++ jour03/exo3.js
@@ -0,0 +1,15 @@
+// Add & Remove
+
+const objects = ['pen', 'book', 'lamp'];
+
+objects.unshift('chair');
+console.log(objects);
+
+objects.pop();
+console.log(objects);
+
+objects.push('laptop');
+console.log(objects);
+
+objects.shift();
+console.log(objects);
