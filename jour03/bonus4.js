f6d6dfa2badb0b0187c5fa51cb9577f40f52575c
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-15 16:29:58 +0100

    Modif bonus4 exercice

--- jour03/bonus4.js
+++ jour03/bonus4.js
@@ -2,18 +2,19 @@ let tableau = [];
 
 for (let i = 1; i <= 20; i++) {
     tableau.push(Math.floor(Math.random() * (100 + 1)));
-
 }
 
 console.log(tableau);
-/*
+
 for (let j = 0; j < 20; j++) {
     tableau.sort((a, b) => a - b);
     if (tableau[j] === tableau[19]) {
         console.log(tableau[j]);
     }
 }
-*/
+
+// METHODE 2, moins capillo-tractée :
+
 let max = tableau[0];
 
 for (let k = 0; k < 20; k++) {
