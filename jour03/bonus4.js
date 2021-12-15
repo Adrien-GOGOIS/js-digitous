a086215d63d35df5b92dd54ab40c12d0ac6813a7
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-15 16:27:55 +0100

    add alternative method for bonus4

--- jour03/bonus4.js
+++ jour03/bonus4.js
@@ -6,10 +6,20 @@ for (let i = 1; i <= 20; i++) {
 }
 
 console.log(tableau);
-
+/*
 for (let j = 0; j < 20; j++) {
     tableau.sort((a, b) => a - b);
     if (tableau[j] === tableau[19]) {
         console.log(tableau[j]);
     }
 }
+*/
+let max = tableau[0];
+
+for (let k = 0; k < 20; k++) {
+    if (tableau[k] > max) {
+        max = tableau[k];
+    }
+}
+
+console.log(max);
