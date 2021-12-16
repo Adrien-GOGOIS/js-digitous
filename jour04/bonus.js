4d5598949f5d2c4faa0e04588f3952e642b2c65d
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-16 15:32:47 +0100

    add jou04 bonus I and II

--- jour04/bonus.js
+++ jour04/bonus.js
@@ -13,3 +13,21 @@ function generatePassword(num) {
 }
 
 generatePassword(15);
+
+// BONUS II
+
+function launchDice(numberOfDice) {
+    return numberOfDice * Math.floor(Math.random() * (6 - 1 + 1) + 1);
+}
+
+let joueur1 = launchDice(5);
+let joueur2 = launchDice(5);
+
+if (joueur1 > joueur2) {
+    console.log(`Le joueur 1 a gagné! Score : ${joueur1} contre ${joueur2}`);
+} else if (joueur2 > joueur1) {
+    console.log(`Le joueur 2 a gagné! Score : ${joueur2} contre ${joueur1}`);
+} else {
+    console.log(`Egalité ! Score : ${joueur1} à ${joueur2}`);
+}
+
