6640fb780aa32be57fe665fa0c3c41b6f35a039a
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-16 16:46:34 +0100

    add optionnal method for bonus password exercice

--- jour04/bonus.js
+++ jour04/bonus.js
@@ -1,18 +1,52 @@
 // BONUS 
 
+
 function generatePassword(num) {
+
     if (num < 6 || num > 15) {
         console.log('ERROR');
+
     } else {
         let password = '';
+        // password = Math.random().toString(16).slice(-num); // Melange lettres et nombres à condition de ne pas dépasser 13 caractères
         for (let i = 1; i <= num; i++) {
             password += String.fromCharCode(Math.floor(Math.random() * (90 - 65 + 1) + 65));
+            // password += String.fromCharCode(Math.floor(Math.random() * (57 - 48 + 1) + 48)); // Avec que des chiffres par exemple
         }
+
     console.log(`Votre mot de passe aléatoire : ${password}`);
+
     }
 }
 
-generatePassword(15);
+generatePassword(13);
+
+
+// METHODE 2 --> Composer un MdP aléatorie contenant tous les caractères alphanumériques
+
+function generatePass(num1) {
+
+    if (num1 < 6 || num1 > 15) {
+
+        console.log('ERROR');
+
+    } else {
+
+        let pass = '';
+        let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$';
+
+        for (let i = 1; i <= num1; i++) {
+            var char = Math.floor(Math.random() * (str.length + 1 - 1) + 1);  
+            pass += str.charAt(char);
+        }
+
+    console.log(`Votre mot de passe aléatoire : ${pass}`);
+
+    }
+}
+
+generatePass(13);
+
 
 // BONUS II
 
