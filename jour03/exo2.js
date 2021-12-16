44fc0eb705677d4c2db97276a9f84d0fde3ca682
Adrien GOGOIS <ad.gogois@gmail.com>
2021-12-16 10:32:03 +0100

    Optimisation du code

--- jour03/exo2.js
+++ jour03/exo2.js
@@ -3,3 +3,10 @@
 const ingredients = ['eggs', 'milk', 'butter'];
 console.log(ingredients[1]);
 console.log(ingredients.indexOf('butter'));
+
+let i = 0;
+while (ingredients[i] !== 'butter') {
+    i++;
+}
+
+console.log(i);
--- jour03/exo6.js
+++ jour03/exo6.js
@@ -4,7 +4,17 @@ let sentence = 'Hello Konexio !';
 let letters = sentence.split('');
 let result = [];
 
-for (let i = letters.length; i >= 0 ; i--) {
+for (let i = letters.length - 1; i >= 0 ; i--) {
     result.push(letters[i]);
 }
 console.log(result.join(''));
+
+// Methode 2
+
+let result1 = "";
+
+for (let i = sentence.length - 1; i >= 0; i--) {
+    result1 += sentence[i];
+}
+
+console.log(result1);
