60814bfa7d745089af2f15ccab2668156edda35b
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-05 16:11:43 +0100

    Add exercice04 proposition

---
+++ Node_NPM_Prompt/Exercice04.js/index.js
@@ -0,0 +1,33 @@
+const prompt = require('prompt');
+
+prompt.start();
+
+const mysteryNum = Math.floor(Math.random() * (100 + 1));
+
+function play() {
+
+    prompt.get({name : 'number', description : 'Quel est le nombre mystère?'}, function(err, res) {
+        
+        let num = parseInt(Object.values(res));
+
+        if (isNaN(num)) {
+            console.log('ERROR');
+            play();
+        }
+
+        if (mysteryNum === num) {
+            console.log('Bravo!!');
+
+        } else if (mysteryNum < num) {
+            console.log('C\'est moins!');
+            play();
+
+        } else if (mysteryNum > num) {
+            console.log('C\'est plus!');
+            play();
+        }
+    })
+
+}
+
+play();
