e676e7129c7ce0957e17d21f825bba9b7f85d903
Adrien GOGOIS <ad.gogois@gmail.com>
2022-01-06 15:20:53 +0100

    Add exo3

---
+++ jour06/exo3/exo3.js
@@ -0,0 +1,54 @@
+const prompt = require('prompt');
+
+prompt.start();
+
+function checkProfile() {
+
+    prompt.get({name : 'email', description : 'Entrez votre email'}, function(err, res) {
+
+        let userMail = Object.values(res);
+        let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
+
+        if (!regex.test(userMail)) {
+            console.log('ERROR');
+            checkProfile();
+        } 
+        
+        else {
+
+        prompt.get({name : 'pseudo', description : 'Entrez votre pseudo'}, function(err, res) {
+
+            let username = Object.values('pseudo');
+            let regex1 = new RegExp('^([0-9a-zA-Z-])$');
+
+            if (regex1.test(username)) {
+                return console.log('ERROR');
+                checkProfile();
+            }
+
+            else {
+
+                prompt.get({name : 'mdp', description : 'Entrez votre mot de passe'}, function(err, res) {
+
+                    let userPassword = Object.values('mdp');
+                    let regex2 = new RegExp('^([0-9a-zA-Z-]){6}$');
+
+                    if (regex2.test(userPassword)) {
+                        console.log('ERROR');
+                        checkProfile();
+                    }
+
+                    console.log('All good!');
+
+                });
+
+            }
+
+        })
+    
+    }
+    })
+
+}
+
+checkProfile();
